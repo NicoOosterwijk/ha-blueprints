90526acec09dafe9676672eba1d5e56ada25b19b
Nico Oosterwijk <noosterwijk@hotmail.com>
2026-01-18 19:01:50 +0100

    Update set_state.py

--- automation/set_state.py
+++ automation/set_state.py
@@ -1,18 +1,27 @@
+#==================================================================================================
+#  python_scripts/set_state.py 
+#==================================================================================================
+
 #--------------------------------------------------------------------------------------------------
 # Set the state or other attributes for the entity specified in the Automation Action
 #--------------------------------------------------------------------------------------------------
 
 inputEntity = data.get('entity_id')
-inputStateObject = hass.states.get(inputEntity)
-inputState = inputStateObject.state
-inputAttributesObject = inputStateObject.attributes.copy()
+if inputEntity is None:
+    logger.warning("===== entity_id is required if you want to set something.")
+else:    
+    inputStateObject = hass.states.get(inputEntity)
+    inputState = inputStateObject.state
+    inputAttributesObject = inputStateObject.attributes.copy()
 
-newState = data.get('state')
-if newState is not None:
-    inputState = newState
-
-newClass = data.get('device_class')
-if newClass is not None:
-    inputAttributesObject['device_class'] = newClass
-
-hass.states.set(inputEntity, inputState, inputAttributesObject)
+    for item in data:
+        newAttribute = data.get(item)
+        logger.debug("===== item = {0}; value = {1}".format(item,newAttribute))
+        if item == 'entity_id':
+            continue            # already handled
+        elif item == 'state':
+            inputState = newAttribute
+        else:
+            inputAttributesObject[item] = newAttribute
+        
+    hass.states.set(inputEntity, inputState, inputAttributesObject)
